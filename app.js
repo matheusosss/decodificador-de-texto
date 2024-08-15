9c83865436eb5ffc13602c49a29e9a0eb056648b
Matheus <matheuszullo@gmail.com>
2024-08-14 22:49:03 -0300

    remocao dos comentarios

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 const textArea = document.querySelector(".text-area");
 const mensagem = document.querySelector(".mensagem");
 
-// A letra "e" é convertida para "enter"
-// A letra "i" é convertida para "imes"
-// A letra "a" é convertida para "ai"
-// A letra "o" é convertida para "ober"
-// A letra "u" é convertida para "ufat"
-
 function btnEncriptar() {
     const textoEncriptado = encriptar(textArea.value);
     mensagem.value = textoEncriptado;
